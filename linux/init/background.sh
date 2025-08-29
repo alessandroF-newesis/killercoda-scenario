3847c50268fa46243bc3cee0ea1d1098348d6480
alessandro <alessandro@newesis.com>
2025-08-29 14:58:39 +0200

    added linux

---
+++ linux/init/background.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+curl -o ricetta.txt https://gist.githubusercontent.com/alessandroF-newesis/991324fe6c01dc77a847b17afdc02c2c/raw/20d81220fae59487634faf859500f19e4fea6543/gistfile1.txt
+
+
+exit 0
+
