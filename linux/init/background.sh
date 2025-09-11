b254b98b22b62bf3fb6eac095ee4a05f7d76bf8c
alessandroF-newesis <94128288+alessandroF-newesis@users.noreply.github.com>
2025-09-11 19:22:40 +0200

    Update background.sh

--- linux/init/background.sh
+++ linux/init/background.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+dd if=/dev/zero of=/var/Bigfile1G bs=1M count=1024
 curl -o ricetta.txt https://gist.githubusercontent.com/alessandroF-newesis/991324fe6c01dc77a847b17afdc02c2c/raw/20d81220fae59487634faf859500f19e4fea6543/gistfile1.txt
 curl -o history.txt https://gist.githubusercontent.com/alessandroF-newesis/6583fa3a279a4a569f0e83892cbe92c2/raw/d2fa62cfbc6425756ee5c4ccc81c125339e55b01/gistfile1.txt
 
