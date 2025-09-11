952214642687b313d1443839686bc32aa89fbbae
alessandroF-newesis <94128288+alessandroF-newesis@users.noreply.github.com>
2025-09-11 19:06:33 +0200

    Update background.sh

--- linux/init/background.sh
+++ linux/init/background.sh
@@ -3,5 +3,5 @@
 curl -o ricetta.txt https://gist.githubusercontent.com/alessandroF-newesis/991324fe6c01dc77a847b17afdc02c2c/raw/20d81220fae59487634faf859500f19e4fea6543/gistfile1.txt
 curl -o history.txt https://gist.githubusercontent.com/alessandroF-newesis/6583fa3a279a4a569f0e83892cbe92c2/raw/d2fa62cfbc6425756ee5c4ccc81c125339e55b01/gistfile1.txt
 
-exit 0
+echo done > /tmp/background0
 
