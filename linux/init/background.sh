b399895057744bfcbb6f6c6e0e156533a288a2d2
alessandroF-newesis <94128288+alessandroF-newesis@users.noreply.github.com>
2025-09-12 09:55:46 +0200

    Update background.sh

--- linux/init/background.sh
+++ linux/init/background.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 dd if=/dev/zero of=/var/Bigfile1G bs=1M count=1024
 
-git clone https://github.com/alessandroF-newesis/killercoda-scenario.git /tmp/repo && \
+git clone --depth 1 https://github.com/alessandroF-newesis/killercoda-scenario.git /tmp/repo && \
 mv /tmp/repo/linux/assets / && \
 rm -rf /tmp/repo
 
