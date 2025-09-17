d34668cf8656eb03acc28afebf745d07437c17e0
alessandroF-newesis <94128288+alessandroF-newesis@users.noreply.github.com>
2025-09-17 15:43:58 +0200

    Update background.sh

--- linux/init/background.sh
+++ linux/init/background.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 mkdir /home/andrea
 dd if=/dev/zero of=/home/andrea/Tempfile bs=1M count=1024
-fallocate -l 2G Tempfile.old.old
-fallocate -l 2G Tempfile.old.old.old
-fallocate -l 2G Tempfile.old.old.old.old
-fallocate -l 2G Tempfile.old.old.old.old.old
-fallocate -l 2G Tempfile.old.old.old.old.old.old
-fallocate -l 2G Tempfile.old.old.old.old.old.old.old
-fallocate -l 2G Tempfile.old.old.old.old.old.old.old.old
+fallocate -l 2G /home/andrea/Tempfile.old.old
+fallocate -l 2G /home/andrea/Tempfile.old.old.old
+fallocate -l 2G /home/andrea/Tempfile.old.old.old.old
+fallocate -l 2G /home/andrea/Tempfile.old.old.old.old.old
+fallocate -l 2G /home/andrea/Tempfile.old.old.old.old.old.old
+fallocate -l 2G /home/andrea/Tempfile.old.old.old.old.old.old.old
 
 git clone --depth 1 https://github.com/alessandroF-newesis/killercoda-scenario.git /tmp/repo && \
 mv /tmp/repo/linux/assets / && \
