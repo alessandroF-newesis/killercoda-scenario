f223a74f94f92eba077044a623e058e92585a343
alessandroF-newesis <94128288+alessandroF-newesis@users.noreply.github.com>
2025-09-17 15:35:40 +0200

    Update background.sh

--- linux/init/background.sh
+++ linux/init/background.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
-dd if=/dev/zero of=/var/Tempfile bs=1M count=1024
-dd if=/dev/zero of=/var/deleteme bs=1M count=1024
-dd if=/dev/zero of=/var/Tempfile.old bs=1M count=1024
-dd if=/dev/zero of=/var/deleteme.tar bs=1M count=1024
-dd if=/dev/zero of=/var/TEmpDeleteme bs=1M count=1024
+mkdir /home/andrea
+dd if=/dev/zero of=/home/andrea/Tempfile bs=1M count=1024
+dd if=/dev/zero of=/home/andrea/deleteme bs=1M count=1024
+dd if=/dev/zero of=/home/andrea/Tempfile.old bs=1M count=1024
+dd if=/dev/zero of=/home/andrea/deleteme.tar bs=1M count=1024
+dd if=/dev/zero of=/home/andrea/TEmpDeleteme bs=1M count=1024
+dd if=/dev/zero of=/home/andrea/Tempfile.old.old bs=1M count=1024
+dd if=/dev/zero of=/home/andrea/Tempfile.old.old.old bs=1M count=1024
+dd if=/dev/zero of=/home/andrea/Tempfile.old.old.tmp bs=1M count=1024
+dd if=/dev/zero of=/home/andrea/Tempfile.old.old.old.tmp bs=1M count=1024
 
 git clone --depth 1 https://github.com/alessandroF-newesis/killercoda-scenario.git /tmp/repo && \
 mv /tmp/repo/linux/assets / && \
