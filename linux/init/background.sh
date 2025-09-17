6a7808c20e21dc795d8fa7bb22e4f23d99db6240
alessandroF-newesis <94128288+alessandroF-newesis@users.noreply.github.com>
2025-09-17 15:42:44 +0200

    Update background.sh

--- linux/init/background.sh
+++ linux/init/background.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 mkdir /home/andrea
 dd if=/dev/zero of=/home/andrea/Tempfile bs=1M count=1024
-dd if=/dev/zero of=/home/andrea/deleteme bs=1M count=1024
-dd if=/dev/zero of=/home/andrea/Tempfile.old bs=1M count=1024
-dd if=/dev/zero of=/home/andrea/deleteme.tar bs=1M count=1024
-dd if=/dev/zero of=/home/andrea/TEmpDeleteme bs=1M count=1024
-dd if=/dev/zero of=/home/andrea/Tempfile.old.old bs=1M count=1024
-dd if=/dev/zero of=/home/andrea/Tempfile.old.old.old bs=1M count=1024
-dd if=/dev/zero of=/home/andrea/Tempfile.old.old.tmp bs=1M count=1024
-dd if=/dev/zero of=/home/andrea/Tempfile.old.old.old.tmp bs=1M count=1024
+fallocate -l 2G Tempfile.old.old
+fallocate -l 2G Tempfile.old.old.old
+fallocate -l 2G Tempfile.old.old.old.old
+fallocate -l 2G Tempfile.old.old.old.old.old
+fallocate -l 2G Tempfile.old.old.old.old.old.old
+fallocate -l 2G Tempfile.old.old.old.old.old.old.old
+fallocate -l 2G Tempfile.old.old.old.old.old.old.old.old
 
 git clone --depth 1 https://github.com/alessandroF-newesis/killercoda-scenario.git /tmp/repo && \
 mv /tmp/repo/linux/assets / && \
