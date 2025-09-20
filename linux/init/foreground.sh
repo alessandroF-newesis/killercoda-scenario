5e4fd94249cf9b619ceac0bcddb3c10b2d9f50f5
alessandroF-newesis <94128288+alessandroF-newesis@users.noreply.github.com>
2025-09-20 17:18:06 +0200

    Update foreground.sh

--- linux/init/foreground.sh
+++ linux/init/foreground.sh
@@ -1,6 +1,6 @@
 while [ ! -f /tmp/background0 ]; do sleep 1; done
 history -r
 ip addr add 100.100.100.100/32 dev lo
-exec -a trallallero nc -l 666 &
-nc -s 100.100.100.100 127.0.0.1 666 &
+exec -a trallallero nc -lk 666 &
+yes "Tun Tun Sahur Cappuccino Assasino" | pv -qL 10 | nc 127.0.0.1 666 &
 clear
