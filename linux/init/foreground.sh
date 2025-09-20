8aa9aeb98c41879108cdd0fa3266b9b98f7f0b5b
alessandroF-newesis <94128288+alessandroF-newesis@users.noreply.github.com>
2025-09-20 17:47:52 +0200

    Update foreground.sh

--- linux/init/foreground.sh
+++ linux/init/foreground.sh
@@ -1,6 +1,3 @@
 while [ ! -f /tmp/background0 ]; do sleep 1; done
 history -r
-ip addr add 100.100.100.100/32 dev lo
-exec -a trallallero nc -l 666 &
-while true; do   echo "Hacking in corso............   download dati delle carte di credito %%%%%";   sleep 3; done | nc 127.0.0.1 666
 clear
